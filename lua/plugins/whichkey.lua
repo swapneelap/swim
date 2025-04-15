214e12fe0ca36f66804ee27977a182e89130fa4e
Swapneel <swapiitr@gmail.com>
2025-04-15 10:33:41 +0200

    feat(whichkey): add lazy manager shortcut to whichkey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,6 +1,11 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
+	opts = {
+		spec = {
+			{ "<leader>l", "<cmd>Lazy<cr>", desc = "Open lazy nvim window", mode = "n" },
+		},
+	},
 	keys = {
 		{
 			"<leader>?",
