54f71236a221ed3d601b35f6a8c91d6a3d388898
Swapneel <swapiitr@gmail.com>
2025-04-15 09:45:32 +0200

    feat(whichkey): add a test keymap

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,4 +1,13 @@
-return { 
+return {
 	"folke/which-key.nvim",
-	lazy = true
+	event = "VeryLazy",
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
