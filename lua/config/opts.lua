992f046f3b2f780911ffe6e3e5f47c725e2d2cd1
Swapneel Amit Pathak <swapiitr@gmail.com>
2025-04-16 11:32:20 +0200

    feat(opts): add ignorecase and termguicolors

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -1,3 +1,13 @@
 vim.opt.number = true
 vim.opt.wrap = false
+
+-- merge system clipboard with nvim clipboard
+-- so now you should be able to copy-paste between
+-- nvim and the system and reverse.
 vim.opt.clipboard = "unnamedplus"
+
+-- igonre case in commands
+vim.opt.ignorecase = true
+
+-- activate true colour support
+vim.opt.termguicolors = true
