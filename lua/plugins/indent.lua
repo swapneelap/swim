b39bdedb5979eeb6019d30ea64f890a260295093
Swapneel Amit Pathak <swapiitr@gmail.com>
2025-04-23 14:34:50 +0200

    feat(indent): add support for indent lines

---
+++ lua/plugins/indent.lua
@@ -0,0 +1,9 @@
+return {
+	"echasnovski/mini.indentscope",
+	version = false,
+	config = function()
+		require('mini.indentscope').setup({
+			symbol = "│",  -- obtained from folke's snacks indent
+		})
+	end,
+}
