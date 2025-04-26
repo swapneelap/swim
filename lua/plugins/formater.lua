f7c829c4c3da9e3a0ceab9f688ad29e24d4b950a
Swapneel Amit Pathak <swapiitr@gmail.com>
2025-04-26 18:08:12 +0200

    fix(ruff,formatter): add options according to ruff docs

--- lua/plugins/formater.lua
+++ lua/plugins/formater.lua
@@ -3,7 +3,14 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			python = { "ruff" },
+			python = {
+				-- To fix auto-fixable lint errors.
+				"ruff_fix",
+				-- To run the Ruff formatter.
+				"ruff_format",
+				-- To organize the imports.
+				"ruff_organize_imports",
+			},
 		},
 	},
 }
