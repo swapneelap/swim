48572b6070c04eff8fc65c696e238a5f57752c33
Swapneel Amit Pathak <swapiitr@gmail.com>
2025-04-27 20:12:21 +0200

    refactor(pylsp,ruff): make sure pylsp and ruff play nice togather
    The main points are:
    - Turn off autocomplete from `ruff`.
    - Turn off linting from `pylsp`.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,1 +1,19 @@
-return { "neovim/nvim-lspconfig" }
+return {
+	"neovim/nvim-lspconfig",
+	init = function()
+		-- NOTE: disable hover from ruff
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if client == nil then
+					return
+				end
+				if client.name == "ruff" then
+					client.server_capabilities.hoverProvider = false
+				end
+			end,
+			desc = "LSP: Disable hover capability from Ruff",
+		})
+	end,
+}
--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -7,6 +7,24 @@ return {
 			function(server_name)
 				require("lspconfig")[server_name].setup({})
 			end,
+			["pylsp"] = function()
+				local lspconfig = require("lspconfig")
+				lspconfig.pylsp.setup({
+					settings = {
+						pylsp = {
+							plugins = {
+								pycodestyle = { enabled = false },
+								pyflakes = { enabled = false },
+								mccabe = { enabled = false },
+								flake8 = { enabled = false },
+
+								-- optional linters
+								pylint = { enabled = false },
+							},
+						},
+					},
+				})
+			end,
 		})
 	end,
 }
