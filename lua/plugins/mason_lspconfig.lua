9937a76c7e3c35ed54b063f525ea523302d329e2
Swapneel Amit Pathak <swapiitr@gmail.com>
2025-05-11 09:53:19 +0200

    fix(mason-lspconfig): move configurations to `vim.lsp.config`
    This is in response to the recent breaking changes in `mason-lspconfig`.
    The version 0.11 of neovim is building a lot of LSP functionality in the
    core package itself, and hence, we move to it as well.

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -1,31 +1,43 @@
 return {
 	"williamboman/mason-lspconfig.nvim",
-	version = "1.*",
 	dependencies = { "williamboman/mason.nvim" },
 	config = function()
 		require("mason-lspconfig").setup()
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				require("lspconfig")[server_name].setup({})
-			end,
-			["pylsp"] = function()
-				local lspconfig = require("lspconfig")
-				lspconfig.pylsp.setup({
-					settings = {
-						pylsp = {
-							plugins = {
-								pycodestyle = { enabled = false },
-								pyflakes = { enabled = false },
-								mccabe = { enabled = false },
-								flake8 = { enabled = false },
-
-								-- optional linters
-								pylint = { enabled = false },
-							},
-						},
+		vim.lsp.config.pylsp = {
+			settings = {
+				pylsp = {
+					plugins = {
+						pycodestyle = { enable = false },
+						pyflakes = { enabled = false },
+						mccabe = { enabled = false },
+						flake8 = { enabled = false },
+						pylint = { enable = false },
 					},
-				})
-			end,
-		})
+				},
+			},
+		}
+		-- require("mason-lspconfig").setup_handlers({
+		-- 	function(server_name)
+		-- 		require("lspconfig")[server_name].setup({})
+		-- 	end,
+		-- 	["pylsp"] = function()
+		-- 		local lspconfig = require("lspconfig")
+		-- 		lspconfig.pylsp.setup({
+		-- 			settings = {
+		-- 				pylsp = {
+		-- 					plugins = {
+		-- 						pycodestyle = { enabled = false },
+		-- 						pyflakes = { enabled = false },
+		-- 						mccabe = { enabled = false },
+		-- 						flake8 = { enabled = false },
+		--
+		-- 						-- optional linters
+		-- 						pylint = { enabled = false },
+		-- 					},
+		-- 				},
+		-- 			},
+		-- 		})
+		-- 	end,
+		-- })
 	end,
 }
