e0347257ca3162ed57874161a9b18afd558d0b0d
Swapneel <swapiitr@gmail.com>
2025-05-16 09:31:42 +0200

    refactor(foldtext): remove folding options

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -34,15 +34,6 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt_local.spell = true
 vim.opt_local.spelllang = { "en_gb" }
 
--- folding options
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-
-vim.opt.foldtext = ""
-
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
-
 --------------------------------------------------------------------
 ---------------------- Auto Commands -------------------------------
 --------------------------------------------------------------------
